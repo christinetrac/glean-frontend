ca05048d630458e2cec9c7f96c2f8d5e7157084f
christinetrac <christineetrac@gmail.com>
2021-03-06 23:08:29 -0500

    fixed add crop ui

--- screens/AddCrop.js
+++ screens/AddCrop.js
@@ -65,28 +65,36 @@ const styles = StyleSheet.create({
         marginTop: 10
       },
       imageWrapper: {
-        width: 400,
-        height: 400,
-        padding: 10,
+        width: 360,
+        height: 334,
         borderColor: '#3A3A3A',
-        borderWidth: 2,
+        borderWidth: 1,
         borderStyle: 'solid',
-        marginTop: 40,
+        marginTop: 20,
         marginBottom: 10,
         position: 'relative',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 15,
+        borderRadius: 8,
+          alignSelf:'center'
       },
       imageContainer: {
-        width: 350,
-        height: 350,
+          width: 360,
+          height: 334,
         position: 'absolute',
-        top: 10,
-        left: 10,
-        bottom: 10,
-        right: 10
+          borderRadius: 8,
       },
+    instructions:{
+        fontSize:12,
+        textAlign:'center',
+        marginTop:5
+    },
+    info:{
+        fontSize:12,
+        textAlign:'center',
+        color:'#959595',
+        width:180
+    },
       predictionWrapper: {
         height: 100,
         width: '100%',
@@ -102,10 +110,21 @@ const styles = StyleSheet.create({
       },
       inputBox: {
           borderStyle: 'solid',
-          borderRadius: 5,
-          borderWidth: 2,
-          borderColor: '#3A3A3A'
+          borderRadius: 8,
+          width:360,
+          height:33,
+          borderWidth: 1,
+          borderColor: '#3A3A3A',
+          alignSelf:'center',
+          marginTop:5,
+          paddingLeft:15
       },
+    inputText: {
+        fontSize:12,
+        color:'#575757',
+        marginLeft:26,
+        marginTop:26
+    },
       button: {
         backgroundColor: '#051034',
         borderRadius: 5,
@@ -114,7 +133,7 @@ const styles = StyleSheet.create({
         alignSelf:'center',
         position:'absolute',
         justifyContent: 'center',
-        bottom:65,
+        bottom:-100,
         zIndex: 99
     },
     buttonText: {
@@ -129,101 +148,102 @@ class ML extends React.Component {
     state = {
       isTfReady: false,
       isModelReady: false,
-      image: null
-    }
-  
+      image: null,
+      yield: null
+    };
+
     async componentDidMount() {
-      await tf.ready()
+      await tf.ready();
       this.setState({
         isTfReady: true
-      })
-      
+      });
+
       const model = require("../assets/model/model.json");
       const weights = require("../assets/model/group1-shard1of1.bin");
       const loadedModel = await tf.loadLayersModel (
         bundleResourceIO(model, weights)
       );
       this.model = loadedModel;
-      
-  
-      this.setState({ isModelReady: true })
+
+
+      this.setState({ isModelReady: true });
       this.getPermissionAsync()
     }
-  
+
     getPermissionAsync = async () => {
       if (Constants.platform.ios) {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
         if (status !== 'granted') {
           alert('Sorry, we need camera roll permissions to make this work!')
         }
       }
-    }
-  
+    };
+
     imageToTensor(rawImageData) {
       const imageData = new Uint8Array(rawImageData);
       const image = decodeJpeg(imageData);
       const resized = tf.image.resizeBilinear(image, [64, 64]);
       const scaled = resized.div(255.0);
       const expanded = scaled.expandDims();
-  
+
       return expanded;
     }
-  
+
     convertProbsToClasses(probs) {
       const classNames = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon'];
-      let res = []
+      let res = [];
       let prob1 = -1;
       let prob2 = -1;
       let prob3 = -1;
       let max1 = 0;
       let max2 = 0;
       let max3 = 0;
-  
+
       for(let i = 0; i < classNames.length; i++) {
         if (probs[i] > prob1) {
           prob1 = probs[i];
           max1 = i;
         }
       }
-  
+
       for(let i = 0; i < classNames.length; i++) {
         if (probs[i] > prob2 && i != max1) {
           prob2 = probs[i];
           max2 = i;
         }
       }
-  
+
       for(let i = 0; i < classNames.length; i++) {
         if (probs[i] > prob3 && i != max1 && i != max2) {
           prob3 = probs[i];
           max3 = i;
         }
       }
-  
+
       res.push({
         className: classNames[max1],
         probability: probs[max1]
       });
-  
+
       res.push({
         className: classNames[max2],
         probability: probs[max2]
       });
-  
+
       res.push({
         className: classNames[max3],
         probability: probs[max3]
       });
-  
+
       return res;
     }
-  
+
     classifyImage = async () => {
       try {
-        const imageAssetPath = Image.resolveAssetSource(this.state.image)
-        const response = await fetch(imageAssetPath.uri, {}, { isBinary: true })
-        const rawImageData = await response.arrayBuffer()
-        const imageTensor = this.imageToTensor(rawImageData)
+        const imageAssetPath = Image.resolveAssetSource(this.state.image);
+        const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
+        const rawImageData = await response.arrayBuffer();
+        const imageTensor = this.imageToTensor(rawImageData);
         const probs = await this.model.predict(imageTensor);
         probs.print();
         probs.softmax().print();
@@ -234,58 +254,62 @@ class ML extends React.Component {
       } catch (error) {
         console.log(error)
       }
-    }
-  
+    };
+
     selectImage = async () => {
       try {
         let response = await ImagePicker.launchImageLibraryAsync({
           mediaTypes: ImagePicker.MediaTypeOptions.All,
           allowsEditing: true,
           aspect: [4, 3]
-        })
-  
+        });
+
         if (!response.cancelled) {
-          const source = { uri: response.uri }
-          this.setState({ image: source })
+          const source = { uri: response.uri };
+          this.setState({ image: source });
           this.classifyImage()
         }
       } catch (error) {
         console.log(error)
       }
-    }
-  
+    };
+
     renderPrediction = prediction => {
       return (
-        
+
         <Text key={prediction.className} style={styles.text}>
           {prediction.className}
         </Text>
       )
     }
-  
+
     render() {
-      const { isTfReady, isModelReady, predictions, image } = this.state
-  
+      const { isTfReady, isModelReady, predictions, image } = this.state;
+
       return (
         <View style={styles.container}>
-          <StatusBar barStyle='light-content' />
           <View style = {styles.imagePickerAndButtons}>
             <TouchableOpacity
                 style={styles.imageWrapper}
                 onPress={isModelReady ? this.selectImage : undefined}>
                 {image && <Image source={image} style={styles.imageContainer} />}
-    
+
                 {isModelReady && !image && (
                 <View>
-                    <Text style={styles.text}>Add a photo of your crop!</Text>
-                    <Text style={{}}>Glean will automatically detect the crop name</Text>
+                    <Icon name='add-photo-alternate' type='material-icons' color={'#4DFFB4'} size={55}/>
+                    <Text style={styles.instructions}>Add a photo of your crop!</Text>
+                    <Text style={styles.info}>Glean will automatically detect the crop name.</Text>
                 </View>
                 )}
             </TouchableOpacity>
-            <Text>Crop Name</Text>
+            <Text style={styles.inputText}>Crop Name</Text>
             <TextInput style={styles.inputBox}>{this.state.cropPrediction}</TextInput>
-            <Text>Yield</Text>
-            <TextInput style={styles.inputBox}></TextInput>
+            <Text style={styles.inputText}>Yield (lbs)</Text>
+            <TextInput
+                style={styles.inputBox}
+                onChangeText={text => this.setState({yield: text})}
+                value={this.state.yield}
+            />
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Save</Text>
             </TouchableOpacity>
